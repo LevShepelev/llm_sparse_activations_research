f614ff00a41ce369f9dde5c6a08d7e7537f7d580
LShepelev <Lev.Shepelev@aton.ru>
2025-07-21 10:29:00 +0300

    fixed bug with eval_strategy

--- src/entry_train.py
+++ src/entry_train.py
@@ -8,7 +8,7 @@ from src.utils import set_seed, ensure_dir, count_trainable_parameters, human_re
 from transformers import TrainerCallback
 import yaml
 import mlflow, os, time
-from src.callbacks import PerplexityAndMLflowCallback, EarlyStoppingCallback
+from src.callbacks import PerplexityAndMLflowCallback
 
 from transformers import Trainer
 
@@ -79,12 +79,11 @@ def main():
             data_collator=data_collator,
         )
 
-        trainer.add_callback(PerplexityAndMLflowCallback(mlflow))
-        trainer.add_callback(EarlyStoppingCallback(patience=2, min_delta=0.0))
+        trainer.add_callback(PerplexityAndMLflowCallback())
         print(">>> total_optim_steps =", trainer._get_train_sampler().__len__() //
             (tc.gradient_accumulation_steps))
         print(">>> eval_steps        =", training_args.eval_steps)
-        print(">>> evaluation_strategy =", training_args.evaluation_strategy)
+        print(">>> eval_strategy =", training_args.eval_strategy)
 
         trainer.train()
         final_metrics = trainer.evaluate()
